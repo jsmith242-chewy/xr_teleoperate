69bf3155e2086f4aef5b833c088838f6bc74c757
Olivia Ordonez <omccaffert@chewy.com>
2025-11-11 09:17:55 -0500

    exception handling added for locomotion with meta quest

--- teleop/teleop_hand_and_arm.py
+++ teleop/teleop_hand_and_arm.py
@@ -349,10 +349,12 @@ if __name__ == '__main__':
                 if tele_data.tele_state.left_thumbstick_state and tele_data.tele_state.right_thumbstick_state:
                     sport_client.Damp()
                 # control, limit velocity to within 0.3
-                sport_client.Move(-tele_data.tele_state.left_thumbstick_value[1]  * 0.3,
+                try:
+                    sport_client.Move(-tele_data.tele_state.left_thumbstick_value[1]  * 0.3,
                                   -tele_data.tele_state.left_thumbstick_value[0]  * 0.3,
                                   -tele_data.tele_state.right_thumbstick_value[0] * 0.3)
-
+                except Exception as e:
+                    print(f"an exception has happened, {e}")
             # get current robot state data.
             current_lr_arm_q  = arm_ctrl.get_current_dual_arm_q()
             current_lr_arm_dq = arm_ctrl.get_current_dual_arm_dq()
